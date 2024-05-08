f5d5d76013a127a23eb9fadffbf04a640399b14b
Pablo A. Bernal <pablo@LAPTOP-VT8PP0CJ>
2024-05-08 15:03:00 +0200

    Documentación de la clase Principal

--- Prueba/src/jcolonia/daw2023/prueba/Principal.java
+++ Prueba/src/jcolonia/daw2023/prueba/Principal.java
@@ -1,7 +1,15 @@
 package jcolonia.daw2023.prueba;
 
+/**
+ * Clase principal
+ */
 public class Principal {
 
+	/**
+	 * Imprimirá por pantalla el texto "Hola mundo"
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String texto = "Hola mundo";
 		
